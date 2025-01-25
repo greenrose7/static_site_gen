44a1d44540a6cf436b594b7a4aae9d3d2a5f8b04
greenrose7 <lucascevans@live.com>
2025-01-25 00:02:41 -0400

    added text_node_to_html_node func and tests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from textnode import TextNode, TextType
+from leafnode import LeafNode
 
 def main():
     textnode = TextNode("This is dummy text", TextType.ITALIC, "http://google.com")
     print(textnode)
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("TextNode has no matching TextType")
 
 main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,8 @@
 import unittest
 
 from textnode import TextNode, TextType
+from main import text_node_to_html_node
+from leafnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -29,6 +31,33 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.ITALIC,)
         self.assertNotEqual(node1, node2)
 
+    def test_text_node_to_html_bold(self):
+        node1 = TextNode("This is a bold node", TextType.BOLD)
+        node1 = text_node_to_html_node(node1)
+        expected_outcome = LeafNode("b", "This is a bold node")
+        self.assertEqual(node1, expected_outcome)
+    
+    def test_text_node_to_html_link(self):
+        node1 = TextNode("This is a link node", TextType.LINK, "https://google.com")
+        node1 = text_node_to_html_node(node1)
+        expected_outcome = LeafNode("a", "This is a link node", {"href": "https://google.com"})
+        self.assertEqual(node1, expected_outcome)
+    
+    def test_text_node_to_html_text(self):
+        node1 = TextNode("This is a text node", TextType.TEXT)
+        node1 = text_node_to_html_node(node1)
+        expected_outcome = LeafNode(None, "This is a text node")
+        self.assertEqual(node1, expected_outcome)
+    
+    def test_text_node_to_html_image(self):
+        node1 = TextNode("This is a image node", TextType.IMAGE, "some/image/location")
+        node1 = text_node_to_html_node(node1)
+        expected_outcome = LeafNode("img", "", {"src": "some/image/location", "alt": "This is a image node"})
+        self.assertEqual(node1, expected_outcome)
+    
+    def test_text_node_to_html_incorrect_textype(self):
+        node1 = TextNode("This is a bogus node", "texttype")
+        self.assertRaises(Exception, text_node_to_html_node, node1)
 
 if __name__ == "__main__":
     unittest.main()
